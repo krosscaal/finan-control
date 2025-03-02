8fb18338e7219a5a28a52bf5af903d39da92cc63
Krossby <krosscaal@gmail.com>
2025-03-02 17:37:10 -0300

    initial commit

---
+++ src/main/java/financial_control_api/model/Categoria.java
@@ -0,0 +1,26 @@
+/*
+ * Author: Krossby Adhemar Camacho Alviz
+ * owned by Krossft.
+ */
+
+package financial_control_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+}
