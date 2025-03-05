9caa6294bd8fe691a0c5e2410473ab8bfee2a5a4
Krossby <krosscaal@gmail.com>
2025-03-05 19:45:25 -0300

    update application properties and Categoria

--- src/main/java/financial_control_api/model/Categoria.java
+++ src/main/java/financial_control_api/model/Categoria.java
@@ -11,6 +11,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -22,5 +26,37 @@ public class Categoria {
     private Long id;
 
     @Column(nullable = false, length = 100)
+    @NotNull
+    @NotBlank
     private String nome;
+
+    public Categoria() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
