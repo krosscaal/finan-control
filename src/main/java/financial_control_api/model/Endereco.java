44c590f7c9344881be2317f45418cebf79fed2af
Krossby <krosscaal@gmail.com>
2025-03-08 23:09:00 -0300

    feat entidade Endereco

---
+++ src/main/java/financial_control_api/model/Endereco.java
@@ -0,0 +1,115 @@
+/*
+ * Author: Krossby Adhemar Camacho Alviz
+ * owned by Krossft.
+ */
+
+package financial_control_api.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Size;
+
+@Embeddable
+public class Endereco {
+    @Size(max = 200)
+    private String logradouro;
+
+    @Size(max = 10)
+    private String numero;
+
+    @Size(max = 100)
+    private String complemento;
+
+    @Size(max = 100)
+    private String bairro;
+
+    @Size(min = 8, max = 8)
+    private String cep;
+
+    @Size(max = 100)
+    private String cidade;
+
+    @Size(max = 100)
+    private String estado;
+
+    public Endereco() {
+    }
+
+    public Endereco(String logradouro, String numero, String complemento, String bairro, String cep, String cidade, String estado) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "logradouro='" + logradouro + '\'' +
+                ", numero='" + numero + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cep='" + cep + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
+}
